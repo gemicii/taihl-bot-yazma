97e5a272d2df8e6e65e0987f0be01b0f43949cf1
gemicii <114801741+gemicii@users.noreply.github.com>
2022-10-15 11:30:21 +0300

    veri_tipi_donusumleri

--- veri_tipi_donusumleri.py
+++ veri_tipi_donusumleri.py
@@ -1,27 +1,15 @@
-# Kullanıcıdan 2 sayı alıp toplama işlemi yapan program
+x = input("adınız")
+y = input("yaşınız")
+isim= "alperen"
+yas = "14"
 
-x = birinci_sayi = 18
-y = ikinci_sayi = 29
-print(x+y)
+print("adınız: " + isim + ", yaşınız: " + str(yas))
 
-x = float(input("1._sayiyi_girin"))
-y = float(input("2._sayiyi_girin"))
-print(x*y)
-
-# TİP DÖNÜŞTÜRME
-# int'ten float'a
-print(float(x))
-
-# float'tan int'e
-print(int(x))
-# bool'dan str'ye
-print(sinav_basarili_mi) #bool  olarak true
-print(type(str(sinav_basarili_mi))) #str olarak "true"
-
-# int'ten str'ye
-print(str(x))
-
-# str'den int'e
-print(int(ad)) # metin int'e dönüşmez
-sayi = "36"
-print(int(sayi)) #sayı değeri taşıyan str dönüştürülebilir
+# karakter verisinin uzunluğuna bakalım
+print(isim[0])
+print(isim[1])
+print(isim[2])
+print(isim[3])
+print(isim[4])
+print(isim[5])
+print(isim[6])
