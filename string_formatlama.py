3590a1f351e201d6025f34ccc51d34c4105e7ad4
gemicii <114801741+gemicii@users.noreply.github.com>
2022-10-15 12:07:24 +0300

    string_formatlama

---
+++ string_formatlama.py
@@ -0,0 +1,8 @@
+# kullanıcıdan isim ,soyad ve yaş bilgilerini alalım
+isim = input("İsminiz:")
+soyad = input("Soyadınız:")
+yas = input("Yaşınız:")
+
+# Kullanıcı bilgilerini yazdıralım
+print("Adınız: " + isim + " " + soyad + ", Yaşınız: " +yas)
+print(f"Adınız: {isim} {soyad}, yaşınız: {yas}")
