660f3e6490e0dc9b3409bcb4f42707b1eb61588b
Gemici25 <alpereng2534@gmail.com>
2022-12-24 12:06:23 +0300

    silinmiş

--- git.py
+++
@@ -1,7 +0,0 @@
-x=sayi_1 =int(input( "birinci sayıyı girin: "))
-y=sayi_2 =int(input( "ikinci sayıyı girin: "))
-        
-print(x+y)
-exit("Bitti")
-
-            
--- string_formatlama.py
+++
@@ -1,8 +0,0 @@
-# kullanıcıdan isim ,soyad ve yaş bilgilerini alalım
-isim = input("İsminiz:")
-soyad = input("Soyadınız:")
-yas = input("Yaşınız:")
-
-# Kullanıcı bilgilerini yazdıralım
-print("Adınız: " + isim + " " + soyad + ", Yaşınız: " +yas)
-print(f"Adınız: {isim} {soyad}, yaşınız: {yas}")
